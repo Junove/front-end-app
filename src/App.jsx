fb7187a9ea6727ef808ad75ffd1802cdf7e6ed62
Junove <nathaniel.endick@gmail.com>
2024-07-30 16:16:40 -0400

    Initial CSS setup + HTML setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,85 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let customers = [
+    {"name":"Name1", "email":"email1@gmail.com", "password":"mypassword1"},
+    {"name":"Name2", "email":"email2@gmail.com", "password":"mypassword2"},
+    {"name":"Name3", "email":"email3@gmail.com", "password":"mypassword3"},
+  ];
+
+  function onDeleteClick(e) {
+    e.preventDefault();
+    console.log("onDeleteClick");
+  }
+
+  function onSaveClick(e) {
+    e.preventDefault();
+    console.log("onSaveClick");
+  }
+
+  function onCancelClick(e) {
+    e.preventDefault();
+    console.log("onCancelClick");
+  }
+
+  function handleListClick() {
+    console.log("handleListClick");
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>React Frontend Application</h1>
+      <div id="content">
+        <div className="container">
+          <table>
+            <caption>Customer List</caption>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Pass</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                customers.map((customer) => (
+                  <tr onClick={()=>handleListClick()}>
+                    <td>{customer.name}</td>
+                    <td>{customer.email}</td>
+                    <td>{customer.password}</td>
+                  </tr>
+                ))
+              }
+            </tbody>
+          </table>
+        </div>
+        <div>
+          <form className="container">
+            <table>
+              <caption>Add</caption>
+              <tbody>
+                <tr>
+                  <td><label for="name">Name:</label></td>
+                  <td><input type="text" id="name" name="name"></input></td>
+                </tr>
+                <tr>
+                  <td><label tor="email">Email:</label></td>
+                  <td><input type="text" id="email" name="email"></input></td>
+                </tr>
+                <tr>
+                  <td><label tor="password">Password:</label></td>
+                  <td><input type="text" id="password" name="password"></input></td>
+                </tr>
+              </tbody>
+            </table>
+          <div>
+            <button onClick={(e)=>onDeleteClick(e)}>Delete</button>
+            <button onClick={(e)=>onSaveClick(e)}>Save</button>
+            <button onClick={(e)=>onCancelClick(e)}>Cancel</button>
+          </div>
+        </form>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
